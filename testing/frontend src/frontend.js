82e44dad0fb82abff22b95e81e9e9fb9c0cde403
Makayla Nham <mdnham@ucsd.edu>
2021-11-22 00:39:37 -0800

    Added code to populate recipe page popup

--- testing/frontend src/frontend.js
+++ testing/frontend src/frontend.js
@@ -57,6 +57,58 @@ async function init(){
         
     }
     
+    // populating the recipe page popup
+    // first, i need to grab all the recipe cards, if they share a class name, that would help
+    // will write a for loop that loops through each card to add an event listener
+    /*
+    recipeCards[i].addEventListener('click', function() {
+        // how to access the specific json file
+        // populate the tags
+        let tagsSection = document.getElementById('recipe-tags');
+        let tagsList = get('tags');
+        if(tagsList.length == 0) {
+            let noTags = document.createElement('p');
+            noTags.innerHTML = 'There are no tags.';
+            tagsSection.appendChild(noTags);
+        }
+        else {
+            for(let i = 0; i < tagsList.length; i++) {
+                let newTag = document.createElement('p');
+                newTag.classList.add('tag');
+                newTag.innerHTML = ` ${tagsList[i]} `;
+                tagsSection.appendChild(newTag);
+            }
+        }
+        // populate recipe name
+        let recipeName = document.getElementById('recipe-name');
+        recipeName.innerHTML = get('name');
+        // populate tracker
+        let trackerCount = document.getElementById('tracker-count');
+        trackerCount.innerHTML = get('count'); // backend needs to include this in the recipe object
+        let lastMade = document.getElementById('tracker-date');
+        lastMade.innerHTML = get('made');
+        // populate image
+        let recipeImage = document.getElementById('recipe-image');
+        recipeImage.setAttribute('src', get('image'));
+        // populate ingredients
+        // needs to change default value of the slider to the number of servings
+        let ingredientList = document.getElementById('ingredients');
+        let ingList = get('ingredients')['ingredients'];
+        for(let i = 0; i < ingList.length; i++) {
+            let newIng = document.createElement('li');
+            newIng.innerHTML = `${ingList[i]['amount']} ${ingList[i]['unit']} of ${ingList[i]['ingName']}`;
+            ingredientList.appendChild(newIng);
+        }
+        // populate instructions
+        let instructionList = document.getElementById('instructions');
+        let instList = get('steps'); // backend needs to fix steps for recipe object
+        for(let i = 0; i < instList.length; i++) {
+            let newInst = document.createElement('li');
+            newInst.innerHTML = instList[i];
+            instructionList.appendChild(newInst);
+        }
+    });
+    */
 
     // const deleteButton = document.getElementById('delete-yes');
     // deleteButton.addEventListener('click', (event) => {
